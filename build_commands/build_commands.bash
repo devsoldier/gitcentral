5b18ae412a558e3b74db24b75d9b10f07cb30ec5
devsoldier <yazeedwong@gmail.com>
2024-05-29 23:16:57 +0800

    added build commands, added permission to android xml

---
+++ build_commands/build_commands.bash
@@ -0,0 +1,6 @@
+fvm flutter build apk \
+--release \
+--dart-define=AUTH_URL=https://github.com \
+--dart-define=API_URL=https://api.github.com \
+--dart-define=CLIENT_ID=Ov23liyaNojm6PhFoS55 \
+--dart-define=CLIENT_KEY=1e915817e86a89fe968001a7249a440600ee263c
