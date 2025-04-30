366f3121c8e1c29db72fe67340f965442438c73e
devsoldier <yazeedwong@gmail.com>
2025-04-30 14:06:30 +0800

    updated ui

--- lib/shared/utils/custom_widgets/custom_elevated_button.dart
+++ lib/shared/utils/custom_widgets/custom_elevated_button.dart
@@ -7,14 +7,12 @@ class CustomElevatedButton extends StatefulWidget {
   final void Function()? onPressed;
   final Widget child;
   final ButtonStyle? style;
-  final double? widthFactor;
   final double? width;
   const CustomElevatedButton({
     Key? key,
     this.onPressed,
     required this.child,
     this.style,
-    this.widthFactor,
     this.width,
   }) : super(key: key);
 
@@ -45,91 +43,88 @@ class _CustomElevatedButtonState extends State<CustomElevatedButton>
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-    return SizedBox(
-      height: 58,
-      width: widget.width ?? screenWidth * (widget.widthFactor ?? 1.0),
-      child: Stack(
-        children: [
-          /// Enabled State
-          if (widget.onPressed != null)
-            Positioned(
-              top: offset.dx,
-              left: offset.dy,
-              child: SizedBox(
-                height: 53,
-                width:
-                    widget.width ?? screenWidth * (widget.widthFactor ?? 1.0),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10.0),
+    return LayoutBuilder(builder: (context, constraints) {
+      return SizedBox(
+        height: 60,
+        width: widget.width ?? constraints.maxWidth,
+        child: Stack(
+          children: [
+            /// Enabled State
+            if (widget.onPressed != null)
+              Positioned(
+                top: offset.dx,
+                left: offset.dy,
+                child: SizedBox(
+                  height: 53,
+                  width: widget.width ?? constraints.maxWidth,
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: horizontalPadding),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10.0),
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-          if (widget.onPressed != null)
-            AnimatedPositioned(
-              top: isPressed ? offset.dx : 0,
-              left: isPressed ? offset.dy : 0,
-              duration: animationDuration,
-              child: SizedBox(
-                height: 53,
-                width:
-                    widget.width ?? screenWidth * (widget.widthFactor ?? 1.0),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
-                  child: GestureDetector(
-                    onTapDown: (_) async {
-                      await pressedOrReleaseAnimation();
-                    },
-                    onTapUp: (_) async {
-                      await pressedOrReleaseAnimation();
-                    },
-                    onTapCancel: () async {
-                      await pressedOrReleaseAnimation();
-                    },
+            if (widget.onPressed != null)
+              AnimatedPositioned(
+                top: isPressed ? offset.dx : 0,
+                left: isPressed ? offset.dy : 0,
+                duration: animationDuration,
+                child: SizedBox(
+                  height: 53,
+                  width: widget.width ?? constraints.maxWidth,
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: horizontalPadding),
+                    child: GestureDetector(
+                      onTapDown: (_) async => await pressedOrReleaseAnimation(),
+                      onTapUp: (_) async => await pressedOrReleaseAnimation(),
+                      onTapCancel: () async =>
+                          await pressedOrReleaseAnimation(),
+                      child: ElevatedButton(
+                        style: widget.style,
+                        onPressed: (widget.onPressed != null)
+                            ? () async {
+                                await onTap();
+                                if (widget.onPressed == null) return;
+                                widget.onPressed!();
+                              }
+                            : null,
+                        child: widget.child,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+            /// Disabled State
+            if (widget.onPressed == null)
+              Positioned(
+                top: offset.dx,
+                left: offset.dy,
+                child: SizedBox(
+                  height: 53,
+                  width: widget.width ?? constraints.maxWidth,
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: horizontalPadding),
                     child: ElevatedButton(
-                      style: widget.style,
-                      onPressed: (widget.onPressed != null)
-                          ? () async {
-                              await onTap();
-                              if (widget.onPressed == null) return;
-                              widget.onPressed!();
-                            }
-                          : null,
+                      style: ElevatedButton.styleFrom(side: BorderSide.none),
+                      onPressed: null,
                       child: widget.child,
                     ),
                   ),
                 ),
               ),
-            ),
-
-          /// Disabled State
-          if (widget.onPressed == null)
-            Positioned(
-              top: offset.dx,
-              left: offset.dy,
-              child: SizedBox(
-                height: 53,
-                width: screenWidth * (widget.widthFactor ?? 1.0),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(side: BorderSide.none),
-                    onPressed: null,
-                    child: widget.child,
-                  ),
-                ),
-              ),
-            ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
+    });
   }
 }
